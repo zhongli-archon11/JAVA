21070665085354492450667093369eafeb36eb9d
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-11-09 21:45:48 +0530

    Method overriding

---
+++ override1.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+class super1
+{
+    int x;
+    super1(int x)
+    {
+        this.x=x;
+    }
+    void display()
+    {
+        System.out.println("Super x: "+x);
+    }
+}
+
+class sub1 extends super1
+{
+    int y;
+    sub1(int x, int y)
+    {
+        super(x);
+        this.y=y;
+    }
+    void display()
+    {
+        System.out.println("y= "+y);
+    }
+}
+class override1
+{
+    public static void main(String args[])
+    {
+        sub1 s1=new sub1(10,20);
+        s1.display();
+    }
+}
