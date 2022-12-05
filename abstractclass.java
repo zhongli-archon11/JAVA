ca29137119edd81a4404cc64cb5a723b2b23015d
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-12-05 12:17:29 +0530

    To implement abstract class and methods

--- abstractclass.java
+++ abstractclass.java
@@ -1,49 +1,49 @@
+
 abstract class Quadrilateral
 {
-	double s1,s2,s3,s4;
-	public Quadrilateral(double side1, double side2, double side3,double side4)
+	double S1, S2, S3, S4;
+	public Quadrilateral(double side1, double side2, double side3, double side4)
 	{
-		s1=side1;
-		s2=side2;
-		s3=side3;
-		s4=side4;
-	}
-	public abstract double computation();
-}
-
+		S1 = side1;
+		S2 = side2;
+		S3 = side3;
+		S4 = side4;
+       }
+	public abstract double ComputeArea();
+};
 class Square extends Quadrilateral
 {
 	public Square(double side1)
 	{
-		super(side1, side2, side3, side4);
+		super(side1, side1, side1, side1);
 	}
-	public double computeArea()
+	public double ComputeArea()
 	{
-		return(s1*s2);
+		return(S1*S1);
 	}
-}
-
+};
 class Rectangle extends Quadrilateral
 {
 	public Rectangle(double side1, double side2)
 	{
-		super(side1, side2, side3, side4);
+		super(side1, side2, side1, side2);
 	}
-	public double computeArea()
+	public double ComputeArea()
 	{
-		return(s1*s2);
+		return(S1 * S2);
 	}
-}
+};
 class abstractclass
 {
 	public static void main(String args[])
 	{
 		Quadrilateral refvar;
-		Square s=new Square(10);
-		Rectangle r=new Rectangle(10, 20);
-		refvar=s;
-		System.out.println("Area of the square= " + refvar.computeArea());
-		refvar=r;
-		System.out.println("Area of the rectangle= " + refvar.computeArea());
+		Square s= new Square(10);
+		Rectangle r= new Rectangle(10, 20);
+		refvar= s;
+		System.out.println("Area of the square= "+refvar.ComputeArea());
+		refvar= r;
+		System.out.println("Area of the recatngle= "+refvar.ComputeArea());
 	}
-}
+};
+                                                                 
